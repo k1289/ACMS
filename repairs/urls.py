5f7dbed4606da178e5aaa528789fe8f7e67f63cd
Likhitha96 <likhitha3996@gmail.com>
2018-03-31 11:49:05 +0530

    Add Login

---
+++ repairs/urls.py
@@ -0,0 +1,9 @@
+#from .models import Products, Service_Centres
+from django.urls import include, path
+from . import views
+
+urlpatterns = [
+	path('',views.index,name='index'),
+	path('<int:product_id>/',views.centres,name='centres'),
+	]
+
