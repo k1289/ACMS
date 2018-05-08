c02bbc81555a7b682e8f2703d663e761bf771248
K.Abhigna <kusumba.abhigna@iiitb.org>
2018-05-08 16:46:07 +0530

    Update views.py
    service-centres

--- mysite/api/views.py
+++ mysite/api/views.py
@@ -1,17 +1,16 @@
-from rest_framework.generics import ListAPIView
+from rest_framework.generics import ListAPIView, RetrieveAPIView
 from repairs.models import Products,Service_Centres
-from .serializers import ProductsSerializer,ServiceCentersSerializer
+from .serializers import ProductsSerializer,ServiceCentreSerializer,ProductDetailSerializer
+
 class ProductsListAPIView(ListAPIView):
 	queryset = Products.objects.all()
 	serializer_class=ProductsSerializer
-class ServiceCentersListAPIView(ListAPIView):
+
+class ServiceCentreListAPIView(ListAPIView):
 	queryset = Service_Centres.objects.all()
-	serializer_class=ServiceCentersSerializer
-
-# class CategoryListView(TemplateView):
-#     template_name = "category-list.html"    
-    
-# class CategoryDetailView(TemplateView):
-# 	template_name = "category-detail.html" 
+	serializer_class=ServiceCentreSerializer
 	
-
+class ServiceProductAPIView(RetrieveAPIView):
+	queryset = Products.objects.all()
+	serializer_class=ProductDetailSerializer
+	
