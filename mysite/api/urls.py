0e375a70e62fa631ba074e6cf61939ca5dfe0de1
K.Abhigna <kusumba.abhigna@iiitb.org>
2018-05-18 01:51:29 +0530

    Add files via upload
    urls

--- mysite/api/urls.py
+++ mysite/api/urls.py
@@ -2,10 +2,16 @@ from django.contrib import admin
 from django.urls import include, path
 from .views import (
 	ProductsListAPIView,
-	ServiceCentersListAPIView
+	ServiceCentreListAPIView,
+	ServiceProductAPIView,
+	#OrderCreateAPIView,
+	ServiceRetrieveAPIView
 	)
+
 urlpatterns=[
-	path('products',ProductsListAPIView.as_view(), name='list'),
-	path('servicecenters',ServiceCentersListAPIView.as_view(), name='list'),
-	
+	#path('create/',OrderCreateAPIView.as_view(),name="create"),
+	path('',ProductsListAPIView.as_view(), name="products-api"),
+	path('sc',ServiceCentreListAPIView.as_view(),name="sc"),
+	path('<pk>',ServiceProductAPIView.as_view(),name="list"),
+	path('list/<pk>',ServiceRetrieveAPIView.as_view(),name='product_centre'),
 ]
